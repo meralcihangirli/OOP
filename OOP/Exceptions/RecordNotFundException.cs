fd302b2aadcab1ed1e3812493fcc8cde68542960
meralcihangirli <mcihangirli@gmail.com>
2023-11-07 20:36:57 +0300

    Exceptions

---
+++ OOP/Exceptions/RecordNotFundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    internal class RecordNotFundException:Exception
+    {
+        public RecordNotFundException(string message):base(message) 
+        
+        {
+            
+        }
+    }
+}
